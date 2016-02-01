0d16881ec8e207f1d8cfcc813aefc96d3ef39d4a
Sophia Allen <smlelevich@gmail.com>
2016-02-01 08:24:21 -0800

    updated hw4.py

--- hw4.py
+++ hw4.py
@@ -1,25 +1,7 @@
-'''I Built this program to draw a random walk within 200 pixels of its origin point as part of my python training. Pretty fun! ''' 
+'''I went to a pyLadies workshop last thursday, and learned about iterators! Pretty cool stuff, especially for big data sets.'''
 
-import turtle
-import random
-wn = turtle.Screen()
-alice = turtle.Turtle()
+my_list = range(1,100)
+my_iter = iter(my_list) 
 
-def distBtwPts(x1, y1, x2, y2):
-    asq = (x2 - x1)**2
-    bsq = (y2 - y1)**2
-    dist = math.sqrt(asq + bsq)
-    return dist
-
-def randomWalkInBounds():
-    startx, starty = alice.position() #get start position
-    moves = 0 #moves counter
-    dist = 0 #initialize distance from start
-    while dist < 190: #go no more than 190 from start, so can't go out of bounds.
-        turn = random.randrange(-360, 360)
-        alice.lt(turn)
-        alice.fd(10)
-        moves += 1
-        endx, endy = alice.position() #get new position
-        dist = distBtwPts(startx, starty, endx, endy) #update distance from start
-    print('Times moved:', moves)
+next(my_iter) # >> 1
+next(my_iter) # >> 2
